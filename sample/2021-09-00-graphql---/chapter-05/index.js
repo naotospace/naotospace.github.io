e21d76747b74c45828e0f5b4393fe08fcc44b3f9
Naoto KISHINO <KishinoNaoto@LIFULL.com>
2021-09-22 09:30:23 +0900

    Implement server v1 query

---
+++ sample/2021-09-00-graphql---/chapter-05/index.js
@@ -0,0 +1,25 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+    type Query {
+        totalPhotos: Int!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        totalPhotos: () => 42
+    },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
